28d218f24d73e20e0df56aa465fa5cf8c33d17cd
pepengenharia <153622010+pepengenharia@users.noreply.github.com>
2024-01-11 19:35:50 -0300

    feat: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        URL_API: "http://localhost:3000/api/"
+        URL_API: "https://www.pepengenharia.com.br/api/"
     }
 }
 
